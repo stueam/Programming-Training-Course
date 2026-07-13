1d369c3cc944b31941a0efd94f6edf3e7eca6afd
stueam <18041384700@163.com>
2026-07-13 19:09:38 +0800

    Fix: synchronization of deleted files outside PA1

--- mypro/musicapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Singer, Song, Comment
-
-admin.site.register(Singer)
-admin.site.register(Song)
-admin.site.register(Comment)
